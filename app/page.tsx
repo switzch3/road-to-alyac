31090f2b9312900965125f303f06de8c39970b7a
Ahn Dong Hoon <switzch3@gmail.com>
2023-08-08 22:24:45 +0900

    메타데이터 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
 import Main from "@/app/Main";
 
+export const metadata: Metadata = {
+  title: "메디램프 - 휠체어 이용자의 접근 가능한 약국 찾기",
+};
 export default function Home() {
   return <Main />;
 }
